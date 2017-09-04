634b578c0b21b47ccfcc808447fe39e649c1c44a
Carlos Perez <solracias@gmail.com>
2017-09-03 21:41:30 -0700

    updated index.html

---
+++ server.js
@@ -0,0 +1,40 @@
+// server depenencies
+var express = require("express"); 
+var bodyParser = require("body-parser"); 
+var logger = require("morgan"); 
+var mongoose = require("mongoose");
+var routes = require("./config/api-routes");
+
+// require models schema
+var model = require("./models/model");
+
+// create instance of express and set a port
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// run morgan for logging
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json"}));
+app.use(express.static("public"));
+
+// MongoDB configuration
+mongoose.connect("mongodb://localhost/portfolio");
+var db = mongoose.connection;
+
+db.on("error", function(error){
+    console.log("Mongoose Error: ", error);
+});
+
+db.once("open", function(){
+    console.log("Mongoose connection successful.");
+})
+
+// routes(app);
+
+// listener
+app.listen(PORT, function(){
+    console.log("app listening on port: " + PORT);
+});
